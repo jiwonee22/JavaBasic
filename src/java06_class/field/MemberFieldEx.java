092eba55aa8d312bb3f937a9dd038ee961b25904
'jiwonee22' <'jiwonee22@naver.com'>
2021-07-11 21:53:31 +0900

    editing error

--- src/java06_class/field/MemberFieldEx.java
+++ src/java06_class/field/MemberFieldEx.java
@@ -15,11 +15,11 @@ public class MemberFieldEx {
 		
 		//----------------------------------------------
 		
-//		System.out.println(mf03.getNum());
+//		System.out.println(mf03.getNum()); //인스턴스화 안함, 객체X, 실제 변수공간이 없음
 		
 		//** NullPointerException
 		//		-> null참조값을 이용해 멤버에 접근하려고 하면 발생한다
-		
+		//		-> 예외가 뜨면 ".(참조연산자)" 왼쪽을 확인하면 된다		
 		//		-> 엄청 자주 보이는 예외!! -> 꼭 기억하세요!!
 		
 		System.out.println();
@@ -32,7 +32,7 @@ public class MemberFieldEx {
 		System.out.println(mf05.city);
 
 		mf04.city = "Busan";
-		
+		 
 		System.out.println("--- Busan 대입 후 ---");
 		
 		System.out.println(mf04.city);
@@ -47,6 +47,13 @@ public class MemberFieldEx {
 		System.out.println(MemberField_02.city);
 		
 		//-------------------------------------------
+		
+		System.out.println();
+		System.out.println("----- 지역변수 테스트 -----");
+		
+		MemberField_03 mf06 = new MemberField_03();
+		
+		mf06.method(111); //메소드 호출
 		
 	}
 }
